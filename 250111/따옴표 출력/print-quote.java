51fc5415ddd8595f97a26f602e89db9e37a44fc2
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-11 15:24:14 +0900

    Title: 따옴표 출력, Time: 77ms, Memory: 9MB, Status: Passed - CodeTree

--- 250111/따옴표 출력/print-quote.java
+++ 250111/따옴표 출력/print-quote.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\"Hello\"");
+        System.out.println("He says \"It's a really simple sentence\".");
     }
 }
