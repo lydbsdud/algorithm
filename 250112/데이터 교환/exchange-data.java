651879937217d7e8e74667025ea3d722dd3b4a23
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-12 18:58:17 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250112/데이터 교환/exchange-data.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int d;
+        int e;
+        d=b;
+        b=a;
+        e=c;
+        c=d;
+        a=e;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
