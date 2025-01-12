34dab1e7e3ebc7a92ec0c0806f149aa06b235d25
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-12 18:55:22 +0900

    Title: 변수 값 교체하기 6, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250112/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a =2;
+        int b = 5;
+        int c ;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
