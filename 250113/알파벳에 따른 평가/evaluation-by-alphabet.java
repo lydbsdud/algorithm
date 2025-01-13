89eb4a60c4e0f3059cd4ea006830d292fb1e1261
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-13 22:55:16 +0900

    Title: 알파벳에 따른 평가, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250113/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner (System.in);
+        String ac = sc.next();
+        char a = ac.charAt(0);
+        if (a=='S'){
+            System.out.println("Superior");
+        }
+        else if(a=='A'){
+            System.out.println("Excellent");
+        }
+        else if (a=='B'){
+            System.out.println("Good");
+        }
+        else if (a=='C'){
+            System.out.println("Usually");
+        }
+        else if (a=='D'){
+            System.out.println("Effort");
+        }
+        else{
+            System.out.println("Failure");
+        }
+    }
+}
