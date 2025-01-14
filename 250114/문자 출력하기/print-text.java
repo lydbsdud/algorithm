ab757799a2ead17651647ae3e902fa1dee51d502
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-14 16:05:09 +0900

    Title: 문자 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250114/문자 출력하기/print-text.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (int i =1; i<=8;i++ ){
+            System.out.print(a);
+        }
+    }
+}
