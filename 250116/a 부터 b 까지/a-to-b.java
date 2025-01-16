a90809308357ed9fdfe377b0cbba68c19ed287d4
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-16 23:12:40 +0900

    Title: a 부터 b 까지, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250116/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언
+        int a, b, i;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        i = a;
+
+        // 출력
+        while(i <= b) {
+            System.out.print(i + " ");
+            if(i % 2 == 1)
+                i *= 2;
+            else
+                i += 3;
+        }
+    }
+}
