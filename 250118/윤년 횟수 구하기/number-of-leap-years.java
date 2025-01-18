a5148ad484543258f34b20e0bf924445bf48c603
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-18 14:30:19 +0900

    Title: 윤년 횟수 구하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250118/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for(int i=1; i<=n;i++){
+            if(i%4==0){
+                if(i%100==0 && i%400!=0){
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+}
