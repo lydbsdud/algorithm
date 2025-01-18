4440acf3163173a38e4934a6eac154fc7b140870
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-18 15:09:20 +0900

    Title: 범위 안의 수 2, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250118/범위 안의 수 2/number-in-range-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        for(int i =0;i<10;i++){
+            int n =sc.nextInt();
+            if(n>=0 && n<=200){
+                sum+=n;
+                count++;
+            }
+        }
+        double avg = (double)sum/count;
+        System.out.printf("%d %.1f",sum,avg);
+    }
+}
