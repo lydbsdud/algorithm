d4dc526caca74aef73af02c47c9d9e10666ba48d
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-19 20:31:48 +0900

    Title: ab 사이에 있는 c, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250119/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        boolean x = false;
+        for(int i=a;i<=b;i++){
+            if(i%c==0){
+                x=true;
+            }
+        }
+        if(x==true){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
