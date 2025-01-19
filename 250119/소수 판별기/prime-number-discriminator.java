9e7b0089c7da13a5151c1c3196e606d09ea55ac6
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-19 20:51:31 +0900

    Title: 소수 판별기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250119/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean x = false;
+        for(int i=2; i<n;i++)
+        {
+            if(n%i==0){
+                x=true;
+            }
+        }
+        if(x==true){
+            System.out.print('C');
+        }
+        else{
+            System.out.print('P');
+        }
+    }
+}
