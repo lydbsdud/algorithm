2b9dcd27e80b914c31116e1fc889e36655059c92
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-26 19:56:52 +0900

    Title: 별표 출력하기 12, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250126/별표 출력하기 12/print-star-12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int t=0;t<n;t++){
+            
+                if(i==0||(t%2!=0 && i<=t)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
