aed0880603a7de6ef60fd25ec1e8387b778aa648
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-01-28 15:03:28 +0900

    Title: 숫자 피라미드, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250128/숫자 피라미드/number-pyramid.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int t=0;t<i+1;t++){
+                System.out.print(i+1+" ");
+            }
+            System.out.println();
+        }
+    }
+}
