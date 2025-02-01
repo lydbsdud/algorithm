9ff1152acc4caa344869899bae2953d3279390e4
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-02-01 16:30:03 +0900

    Title: 나눗셈의 나머지, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

--- 250201/나눗셈의 나머지/remainder-of-division.java
+++ 250201/나눗셈의 나머지/remainder-of-division.java
@@ -5,17 +5,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int arrMod[] = new int[5];
+        int arrMod[] = new int[100];
         int arrCount[] = new int[10];
         int sum=0;
-        for(int i=0;i<5;i++){
+        for(int i=0;i<100;i++){
             arrMod[i]=a%b;
             a/=b;
             arrCount[arrMod[i]]++;
+            if(a<=1){
+                break;
+            }
         }
         for(int i=0;i<10;i++){
             arrCount[i]*=arrCount[i];
             sum+=arrCount[i];
+            //System.out.print(arrCount[i]);
         }
         System.out.print(sum);
     }
