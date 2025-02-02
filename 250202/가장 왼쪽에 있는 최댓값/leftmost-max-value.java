4503c415cfc0a710b3ab8d2d1cfacfb4afac24ec
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-02-02 19:24:06 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 216ms, Memory: 12MB, Status: Passed - CodeTree

--- 250202/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 250202/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -4,7 +4,7 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[10];
+        int[] arr = new int[n];
         boolean running = true;
         for(int i=0; i<n;i++){
             arr[i]=sc.nextInt();
