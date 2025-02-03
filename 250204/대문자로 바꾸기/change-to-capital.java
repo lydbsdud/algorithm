f9c171a089c490772184e2adec29e7dac449e2a4
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-02-04 01:35:50 +0900

    Title: 대문자로 바꾸기, Time: 110ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250204/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char[][] arr = new char[5][3];
+        for(int i=0;i<5;i++){
+            for(int j=0;j<3;j++){
+                arr[i][j] = sc.next().charAt(0);
+                System.out.print((char)(arr[i][j]-32)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
