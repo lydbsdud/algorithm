1af20b3d5463a13c5ba8242d1f5833c8a5568137
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-02-05 16:06:42 +0900

    Title: 파스칼의 삼각형, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250205/파스칼의 삼각형/pascals-triangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 1;
+        int[][] arr = new int[n][n];
+    
+        for(int i=0;i<n;i++){
+            for(int j=0;j<x;j++){
+                if(j==x-1 || j==0){
+                    arr[i][j]=1;
+                }else{
+                    arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+                }
+                System.out.print(arr[i][j]+" ");
+            }
+            x++;
+            System.out.println();
+        }
+    }
+}
