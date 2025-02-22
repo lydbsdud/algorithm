00ddf812086d590b380bb9fbd9c852c541a7f5d2
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-02-22 19:47:42 +0900

    Title: 문자열 범위 출력하기 2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 250222/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 250222/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -5,12 +5,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String st = sc.next();
         int n = sc.nextInt();
-        char[] arr = new char[100];
+        char[] arr = new char[101];
         for(int i=0;i<st.length();i++){
             arr[i]=st.charAt(i);
         }
-        for(int i=st.length()-1;i>=st.length()-n;i--){
+        if(n<=st.length()){
+            for(int i=st.length()-1;i>=st.length()-n;i--){
             System.out.print(arr[i]);
+            }
+        }else{
+            for(int i=st.length()-1;i>=0;i--){
+            System.out.print(arr[i]);
+            }
         }
     }
 }
