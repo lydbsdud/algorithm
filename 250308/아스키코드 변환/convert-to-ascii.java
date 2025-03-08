32657a7b8cc9ff33c00e220af54245b1923fcce1
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-08 21:13:59 +0900

    Title: 아스키코드 변환, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250308/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        int n = sc.nextInt();
+        System.out.print((int)a+" "+(char)n);
+    }
+}
