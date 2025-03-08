647a75adee49c4be9799e1e6784f34f760c3df91
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-08 21:24:21 +0900

    Title: 그 전 알파벳, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250308/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char x = sc.next().charAt(0);
+        if(x=='a'){
+            System.out.print('z');
+        }else{
+            System.out.print((char)(x-1));
+        }
+    }
+}
