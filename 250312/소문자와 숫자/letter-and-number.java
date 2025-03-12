35843b646f9b43f5faa7c765cca22abfe9bb5590
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-12 16:48:09 +0900

    Title: 소문자와 숫자, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250312/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        StringBuilder result = new StringBuilder();
+        for(char ch: st.toCharArray()){
+            if(Character.isLetter(ch)){
+                result.append(Character.toLowerCase(ch));
+            }           
+            if(Character.isDigit(ch)){
+                result.append(ch);
+            }
+        }
+        System.out.print(result);
+        
+    }
+}
