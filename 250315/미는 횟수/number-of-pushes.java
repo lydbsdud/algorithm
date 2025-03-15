6ce27c3d8f4b209130ee837adf9c788551cf9bcd
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-15 16:36:38 +0900

    Title: 미는 횟수, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250315/미는 횟수/number-of-pushes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String st1 = sc.nextLine();
+        String st2 = sc.nextLine();
+        int count =0 ;
+        for(int i=0;i<st1.length()-1;i++){
+            st1 = st1.charAt(st1.length()-1)+st1.substring(0,st1.length()-2);
+            count++;
+            if(st1.equals(st2)){
+                break;
+            }
+        }
+        if(count==0){
+            System.out.print(-1);
+        }
+        else{
+            System.out.print(count);
+        }
+    }
+}
