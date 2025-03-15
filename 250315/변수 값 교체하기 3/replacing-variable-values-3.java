a9ab40e86cb3d895e301dc5df51e5ba6cd951f29
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-15 18:32:03 +0900

    Title: 변수 값 교체하기 3, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250315/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = 3;
+        int b = 5;
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
