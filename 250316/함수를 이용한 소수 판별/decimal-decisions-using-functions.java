ab0a0c46f944d1f01f6112b5a3cec328576f2a3e
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-03-16 18:58:38 +0900

    Title: 함수를 이용한 소수 판별, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 250316/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 250316/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean isMagicNum(int n){
+        if(n==1){
+            return false;
+        }
         for(int i=2;i<n;i++){
             if(n%i==0){
                 return false;
             }
-        }
-        if(n==1){
-            return true;
         }
         return true;
     }
