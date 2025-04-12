11819d6126998f09ef2f79de15b98f2a70184682
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-04-12 20:28:41 +0900

    Title: 큰 수 자리수의 합, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250412/큰 수 자리수의 합/sum-of-large-numeric-digits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static int sum(int n){
+        if(n<10){
+            return n;
+        }
+        return sum(n/10)+(n%10);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Please write your code here.
+        System.out.print(sum(a*b*c));
+    }
+}
