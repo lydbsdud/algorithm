cdcb261be6d28a354bdb0695fd65490762edbb91
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-04-14 21:52:14 +0900

    Title: Next Level, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250414/Next Level/next-level.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class User {
+    String id;
+    int level;
+    public User(){
+        this.id = "codetree";
+        this.level = 10;
+    }
+    public User(String id, int level){
+        this.id = id;
+        this.level = level;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        int level = sc.nextInt();
+        // Please write your code here.
+        User user1 = new User();
+        System.out.println("user "+user1.id+" lv "+user1.level);
+        User user2 = new User(id, level);
+        System.out.println("user "+user2.id+" lv "+user2.level);
+    }
+}
