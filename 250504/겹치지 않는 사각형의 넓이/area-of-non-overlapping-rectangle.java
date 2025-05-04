e88c36d90bb8f530c5d511569c2fb4df454794e1
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-05-04 21:04:54 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250504/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ax1 = sc.nextInt();
+        int ay1 = sc.nextInt();
+        int ax2 = sc.nextInt();
+        int ay2 = sc.nextInt();
+        int bx1 = sc.nextInt();
+        int by1 = sc.nextInt();
+        int bx2 = sc.nextInt();
+        int by2 = sc.nextInt();
+        int mx1 = sc.nextInt();
+        int my1 = sc.nextInt();
+        int mx2 = sc.nextInt();
+        int my2 = sc.nextInt();
+        int w1=(ax2-ax1)*(ay2-ay1);
+        int w2=(bx2-bx1)*(by2-by1);
+        int w3 = F( ax1, ay1, ax2, ay2, mx1, my1, mx2, my2)+F(bx1, by1,bx2,by2, mx1, my1, mx2, my2);
+        System.out.print(w1+w2-w3);
+    }
+
+    public static int F(int ax1, int ay1, int ax2, int ay2,
+    int bx1, int by1, int bx2, int by2){
+        int x = Math.min(ax2,bx2)-Math.max(ax1,bx1);
+        int y = Math.min(ay2,by2)-Math.max(ay1,by1);
+        return x*y;
+    }
+        
+}
