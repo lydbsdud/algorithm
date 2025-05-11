05e41955e7039367e4562c2318ca5f257c28b12c
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-05-11 18:18:09 +0900

    Title: 만나는 그 순간, Time: 129ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250511/만나는 그 순간/the-moment-we-meet.java
+++ 250511/만나는 그 순간/the-moment-we-meet.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
+    static int time = 0;
     public static void main(String[] args) {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] x = W(n);
         int[] y = W(m);
-        for(int i=1;i<1000;i++){
+        for(int i=1;i<=time;i++){
             if(x[i]==y[i]){
                 System.out.print(i);
                 break;
+            }
+            if(i==time){
+                System.out.print(-1);
             }
         }
         
     }
     public static int[] W(int n){
         int[] arr = new int[1000];
-        int time = 0;
+        time = 0;
         int where = 0;
         for(int i=0;i<n;i++){
             char q = sc.next().charAt(0);
