676938bb6e3e0c6ba826f3958ac4304d3ae17387
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-05-16 23:08:07 +0900

    Title: 벌금은 누구에게, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250516/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int[] studentNum = new int [n+1];
+        for(int i=1;i<=n;i++){
+            studentNum[i] = i;
+        }
+        int[] penalizedPerson = new int[m];
+        for (int i = 0; i < m; i++) {
+            penalizedPerson[i] = sc.nextInt();
+            studentNum[penalizedPerson[i]]++;
+            if(studentNum[penalizedPerson[i]]==k){
+                System.out.print(penalizedPerson[i]);
+                break;
+            }else if(i==m-1){
+                System.out.print(-1);
+            }
+        }
+    }
+}
