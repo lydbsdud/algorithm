de83146831b6226ce955f7c2bbeb3bc77098418e
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-06-15 16:02:41 +0900

    Title: 선두를 지켜라 3, Time: 131ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250615/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][2];
+        int[][] b = new int[m][2];
+        int[] A = new int[1000];
+        int[] B = new int[1000];
+        int aT = 0;
+        int aW = 0;
+        int bT = 0;
+        int bW = 0;
+        for (int i = 0; i < n; i++) {
+            a[i][0] = sc.nextInt();
+            a[i][1] = sc.nextInt();
+            for(int j=aT+1;j<=aT+a[i][1];j++){
+                aW+=a[i][0];
+                A[j]=aW;
+            }
+            aT+=a[i][1];
+        }
+        for (int i = 0; i < m; i++) {
+            b[i][0] = sc.nextInt();
+            b[i][1] = sc.nextInt();
+            for(int j=bT+1;j<=bT+b[i][1];j++){
+                bW+=b[i][0];
+                B[j]=bW;
+            }
+            bT+=b[i][1];
+        }
+        int t = Math.max(aT,bT);
+        int count = 0;
+        for(int i=1;i<=t;i++){
+            if((A[i-1]>=B[i-1])&&(A[i]<B[i])){
+                count++;
+                continue;
+            }
+            if((A[i-1]<=B[i-1])&&(A[i]>B[i])){
+                count++;
+                continue;
+            }
+            if((A[i-1]>B[i-1])&&(A[i]<=B[i])){
+                count++;
+                continue;
+            }
+            if((A[i-1]<B[i-1])&&(A[i]>=B[i])){
+                count++;
+                continue;
+            }
+        }
+        System.out.print(count);
+    }
+}
