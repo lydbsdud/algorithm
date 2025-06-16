943a0075a32009c1c8cc55b9576156b0aea2ce79
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-06-16 23:24:07 +0900

    Title: 작은 구슬의 이동, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250616/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int gridNum(char ch){
+        if(ch=='U'){
+            return 3;
+        }
+        else if(ch=='D'){
+            return 0;
+        }
+        else if(ch=='R'){
+            return 1;
+        }
+        return 2;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        String D = sc.next();
+        int[] dx = {1, 0, 0, -1};
+        int[] dy = {0, 1, -1 ,0};
+        int dir = gridNum(D.charAt(0));
+        int nx = R;
+        int ny = C;
+        for (int i=0; i<T;i++){
+            if(ny==4&&dir==1){
+                dir = 2;
+                continue;
+            }
+            if(ny==1&&dir==2){
+                dir = 1;
+                continue;
+            }
+            if(nx==1&&dir==3){
+                dir = 0;
+                continue;
+            }
+            if(nx==4&&dir==0){
+                dir = 3;
+                continue;
+            }
+            nx+=dx[dir];
+            ny+=dy[dir];
+        }
+        System.out.print(nx+" "+ny);
+
+    }
+}
