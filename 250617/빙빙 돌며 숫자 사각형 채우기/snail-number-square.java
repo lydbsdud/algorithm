feacb1d9c1772176980e723c30b46f5ecf6e22d3
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2025-06-17 23:50:34 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250617/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static boolean isGrid(int x, int y, int n, int m){
+        return (x>=0)&&(x<n)&&(y>=0)&&(y<m);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+        int[][] xy = new int[n][m];
+        int dir = 0;
+        int x = 0;
+        int y = 0;
+        xy[0][0] = 1;
+        for(int i=2;i<=n*m;i++){
+            int nx = x+dx[dir];
+            int ny = y+dy[dir];
+
+            if(!isGrid(nx, ny, n, m)||(xy[nx][ny]>0)){
+                dir = (dir+1)%4;   
+            }
+
+            x = x+dx[dir];
+            y = y+dy[dir];
+
+            xy[x][y] = i;
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(xy[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
