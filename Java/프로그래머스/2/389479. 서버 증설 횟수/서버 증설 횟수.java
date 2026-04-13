b013f4d190491346014bbbe6a4666a600e8cb78a
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2026-04-13 15:52:02 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -0,0 +1,26 @@
+// 최대 이용자수 : m 서버 한 대가 운영 가능한 시간 : k
+class Solution {
+    public int solution(int[] players, int m, int k) {
+        int count = 0;
+        int serverCount = 1;
+        int[] arr = new int[24];
+        for(int i=0; i<players.length; i++){
+            if(i-k>=0 && arr[i-k]>0){
+                serverCount -= arr[i-k];
+            }
+            if(players[i] >= serverCount*m ){
+                if(players[i] == serverCount*m){
+                    serverCount++;
+                    count++;
+                    arr[i]++;
+                }else{
+                    int serverplus = (players[i]-(serverCount*m))/m + 1;
+                    serverCount += serverplus;
+                    count += serverplus;
+                    arr[i] += serverplus;
+                }
+            }
+        }
+        return count;
+    }
+}
