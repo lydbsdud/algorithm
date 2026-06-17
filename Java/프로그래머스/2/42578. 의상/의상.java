b5788d0e67e6b0e471054d077a67cbe7955112c5
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2026-06-18 01:15:01 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String[] clothe: clothes){
+            String type = clothe[1];
+            map.put(type, map.getOrDefault(type, 0)+1);
+        }
+        for(int count : map.values()){
+            answer *= (count+1);
+        }
+        return answer-1;
+    }
+}
