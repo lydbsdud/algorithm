a3ea593b7989238563635fce8c5e801f9858ec83
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2026-06-27 02:07:40 +0900

    Time: 232 ms (5.12%), Space: 101 MB (8%) - LeetHub

---
+++ LeetCode/Medium/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -0,0 +1,43 @@
+class RandomizedSet {
+
+    List<Integer> list;
+    Map<Integer, Integer> map;
+    Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int index = map.get(val);
+        int last = list.get(list.size() - 1);
+
+        list.set(index, last);
+        map.put(last, index);
+
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+        return list.get(random.nextInt(list.size()));
+    }
+}
