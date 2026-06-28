4cb6ac9b9000669394f48d6ef89db9e66322ae41
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2026-06-29 01:14:17 +0900

    Time: 2 ms (99.44%), Space: 119.3 MB (64.4%) - LeetHub

---
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start = 0;
+        int tank = 0;
+        int total = 0;
+        int gain = 0;
+        for(int i = 0; i < gas.length; i++) {
+            gain = gas[i] - cost[i];
+
+            tank += gain;
+            total += gain;
+
+            if(tank < 0){
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return total < 0 ? -1 : start;
+    }
+}
