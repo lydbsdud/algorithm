187b8cc11f79d6b596404cf677901c5316a6c686
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2026-06-29 00:11:35 +0900

    Time: 3 ms (26.21%), Space: 64.1 MB (94.66%) - LeetHub

---
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        int l = 1;
+        int r = 1;
+        left[0] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        right[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        for(int i = 0; i < len; i++) {
+            left[i] = left[i] * right[i];
+        }
+        return left;
+    }
+}
