e406fbcf3e014835dfa30368a61be250f2e3f050
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2026-07-08 01:46:23 +0900

    [level 3] Title: 이중우선순위큐, Time: 40.76 ms, Memory: 133 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for (String operation : operations) {
+            String[] command = operation.split(" ");
+            int num = Integer.parseInt(command[1]);
+
+            if (command[0].equals("I")) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                continue;
+            }
+
+            if (map.isEmpty()) {
+                continue;
+            }
+
+            int key = (num == 1) ? map.lastKey() : map.firstKey();
+            int count = map.get(key);
+
+            if (count == 1) {
+                map.remove(key);
+            } else {
+                map.put(key, count - 1);
+            }
+        }
+
+        if (map.isEmpty()) {
+            return new int[]{0, 0};
+        }
+
+        return new int[]{map.lastKey(), map.firstKey()};
+    }
+}
